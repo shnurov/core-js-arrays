04d906117f8ee26f31deaa916f19f46b95feaf98
Pavel Shnurov <pavel.shnurov@gmail.com>
2024-01-13 00:26:00 +0400

    feat: add 19 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -338,8 +338,8 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((acc, item) => acc + item[0] - item[1], 0);
 }
 
 /**
