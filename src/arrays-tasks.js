50eef95a0303a958fec00684c22d3253a9ccf5d5
Pavel Shnurov <pavel.shnurov@gmail.com>
2024-01-13 01:00:03 +0400

    feat: add 20 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -354,8 +354,11 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const chunks = Math.ceil(arr.length / chunkSize);
+  return Array(chunks)
+    .fill(0)
+    .map((chunk, i) => arr.slice(i * chunkSize, i * chunkSize + chunkSize));
 }
 
 /**
