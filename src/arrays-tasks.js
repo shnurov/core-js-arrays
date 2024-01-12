eaba998f29ac9a95af999e6226daef3c3be49e67
Pavel Shnurov <pavel.shnurov@gmail.com>
2024-01-12 17:30:43 +0400

    feat: add 7 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -146,8 +146,8 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  return +arr.reduce((acc, item) => acc + item / arr.length, 0).toFixed(2);
 }
 
 /**
